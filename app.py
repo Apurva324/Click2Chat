a241bde297a516f02c773f726b255f25a6d74978
Apurva Shaw <apurvashaw@Apurvas-MacBook-Air.local>
2026-05-02 01:24:49 +0530

    Fix CORS credentials issue

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ EvalBase.metadata.create_all(bind=eval_engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
